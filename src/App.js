1e55c85e56fe3c966ed0ef7489c5a427bad54862
Deepak Kumar Dash <deepakkumardash313@gmail.com>
2020-04-27 16:52:33 +0530

    added contacts list

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import ListContacts from './ListContacts';
 
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map((person) => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
-}
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ContactList contacts={[
-          {name: 'Tyler'},
-          {name: 'Richard'},
-          {name: 'Karen'}
-        ]}/>
-        <ContactList contacts={[
-          {name: 'Hermoine'},
-          {name: 'Weasley'},
-          {name: 'Harry'}
-        ]} />
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
